98281519a3d7a3e742a532e7f8b7815e83178ab4
Daniel Muckelbauer <dadamuck@hotmail.com>
2021-02-14 17:44:36 +0100

    Unit tests and launch.json

--- tests/playerstatus-updater.spec.js
+++ tests/playerstatus-updater.spec.js
@@ -1,6 +1,7 @@
 import { beforeEach } from '@jest/globals';
 import { DamageCalculator } from '../src/damage-calculator';
 import { PlayerstatsUpdater } from '../src/playerstats-updater';
+import { jest } from '@jest/globals';
 
 let playerstatsUpdater;
 let damageCalculator;
@@ -15,8 +16,32 @@ test('initializes with copy of currently active encounter', () => {
 
     playerstatsUpdater.initialize(activeCombat);
 
-    expect(playerstatsUpdater.copyOfLastCombat)
-        .toEqual(activeCombat);
-    expect(playerstatsUpdater.copyOfLastCombat)
-        .not.toBe(activeCombat);
+    expect(playerstatsUpdater.copyOfLastCombat).toEqual(activeCombat);
+    expect(playerstatsUpdater.copyOfLastCombat).not.toBe(activeCombat);
+});
+
+test('stores a copy of the new combat encounter  ', () => {
+    playerstatsUpdater.copyOfLastCombat = {
+        combatant: {
+            actor: { name: 'lastCombatant' }
+        }
+    };
+    const newCombat = { combatant: 'newCombatant' };
+    jest.spyOn(damageCalculator, 'calculateDamageDealt').mockReturnValue(1);
+    jest.spyOn(damageCalculator, 'calculateDamageTaken').mockReturnValue(1);
+
+    playerstatsUpdater.updatePlayerStats(newCombat);
+
+    expect(playerstatsUpdater.copyOfLastCombat).toEqual(newCombat);
+    expect(playerstatsUpdater.copyOfLastCombat).not.toBe(newCombat);
+});
+
+test('does nothing if combatant is the same', () => {
+    const newCombat = { combatant: 'combatant' };
+    const savedCombat = { combatant: 'combatant' };
+    playerstatsUpdater.copyOfLastCombat = savedCombat;
+
+    playerstatsUpdater.updatePlayerStats(newCombat);
+
+    expect(playerstatsUpdater.copyOfLastCombat).toEqual(savedCombat);   
 });
